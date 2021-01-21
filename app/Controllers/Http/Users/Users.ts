d2bedbc6f98558d1a2e9c80d7f211981c9dfab3a
piassa157 <lindaum98@gmail.com>
2021-01-21 14:19:11 -0300

    [GGP] Feat: Create controller with --resource

---
+++ app/Controllers/Http/Users/Users.ts
@@ -0,0 +1,25 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UsersController {
+  public async index({ }: HttpContextContract) {
+    const data = await 
+  }
+
+  public async create ({}: HttpContextContract) {
+  }
+
+  public async store ({}: HttpContextContract) {
+  }
+
+  public async show ({}: HttpContextContract) {
+  }
+
+  public async edit ({}: HttpContextContract) {
+  }
+
+  public async update ({}: HttpContextContract) {
+  }
+
+  public async destroy ({}: HttpContextContract) {
+  }
+}
